54812ec0eff3a789517be44a823100ed10437687
zoubinnaba <emmanuelzoubinnaba@gmail.com>
2018-08-10 16:58:59 +0000

    Personnalisation de la page d'adminitration de django

--- polls/models.py
+++ polls/models.py
@@ -14,7 +14,10 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-    
+
 def was_published_recently(self):
     now = timezone.now()
     return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
